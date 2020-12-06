ad455506140fc5cc4eb3250c9066d26491c6376d
Philipp Czora <philipp@czora.dev>
2020-12-06 01:08:14 +0100

    Adds solution for day 2 part 1

--- day2/src/main/scala/Main.scala
+++ day2/src/main/scala/Main.scala
@@ -3,14 +3,20 @@ import scala.io.Source
 object Main {
   def main(args: Array[String]): Unit = {
     val data = Source.fromResource("data.txt").getLines.map(parseLine).toList
-    val validPasswordCount = data.filter(l => checkPolicy(l.policy, l.password)).size
+    val validPasswordCount = data.count(l => checkPolicy(l.policy, l.password))
     println(validPasswordCount)
+    val validPasswordCountPart2 = data.count(l => checkPolicyPart2(l.policy, l.password))
+    println(validPasswordCountPart2)
+
+    assert(checkPolicyPart2(Policy(1, 3, 'a'), "abcde"))
+    assert(!checkPolicyPart2(Policy(1, 3, 'b'), "cdefg"))
+    assert(!checkPolicyPart2(Policy(2, 9, 'c'), "ccccccccc"))
   }
 
   def parseLine(line: String): Line = {
     line.split(":") match {
       case Array(policy, password) => {
-        Line(parsePolicy(policy), password)
+        Line(parsePolicy(policy), password.strip)
       }
     }
   }
@@ -25,6 +31,13 @@ object Main {
   def checkPolicy(policy: Policy, password: String): Boolean = {
     val charCount = password.count(char => char == policy.char)
     charCount >= policy.from && charCount <= policy.to
+  }
+
+  def checkPolicyPart2(policy: Policy, password: String): Boolean = {
+    val firstCharMatches = password.charAt(policy.from - 1) == policy.char
+    val secondCharMatches = password.charAt(policy.to - 1) == policy.char
+
+    firstCharMatches ^ secondCharMatches
   }
 }
 
