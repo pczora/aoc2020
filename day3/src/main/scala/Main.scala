fb9edfe96cfaa065923e963c3f3e72467ed0b0b5
Philipp Czora <philipp@czora.dev>
2020-12-06 11:47:53 +0100

    Adds solution for day 3 part 2

--- day3/src/main/scala/Main.scala
+++ day3/src/main/scala/Main.scala
@@ -3,17 +3,32 @@ import scala.io.Source
 object Main {
   def main(args: Array[String]): Unit = {
     val input = Source.fromResource("input.txt").getLines().toList
-    println(ride(0, 0, input))
+
+    // Part 1
+    println(ride(0, 0, 3, 1, 0, input))
+
+    // Part 2
+    val result = ride(0, 0, 1, 1, 0, input).toLong *
+    ride(0, 0, 3, 1, 0, input) *
+    ride(0, 0, 5, 1, 0, input) *
+    ride(0, 0, 7, 1, 0, input) *
+    ride(0, 0, 1, 2, 0, input)
+    println(result)
   }
 
-  def isTree(pos: Int, line: String) = line.charAt(pos % line.length) == '#'
+  def isTree(posX: Int, posY: Int, m: List[String]) = {
+    val line = m(posY)
+    line.charAt(posX % line.length) == '#'
+  }
 
-  def ride(position: Int, treesEncountered: Int, m: List[String]): Int = {
-    m match {
-      case Nil => treesEncountered
-      case x :: xs => if (isTree(position, x)) ride(nextPosition(position, x.length), treesEncountered + 1, xs) else ride(nextPosition(position, x.length), treesEncountered, xs)
+  def ride(posX: Int, posY: Int, deltaX: Int, deltaY: Int, treesEncountered: Int, m: List[String]): Int = {
+    if (posY >= m.size) treesEncountered else {
+      if (isTree(posX, posY, m)) {
+        ride(posX + deltaX, posY + deltaY, deltaX, deltaY, treesEncountered + 1, m)
+      } else {
+        ride(posX + deltaX, posY + deltaY, deltaX, deltaY, treesEncountered, m)
+      }
     }
   }
 
-  def nextPosition(position: Int, lineLength: Int) = (position + 3) % lineLength
 }
