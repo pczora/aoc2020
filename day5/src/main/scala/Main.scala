10b35706955249f104e43a32d0074493dbb7d661
Philipp Czora <philipp@czora.dev>
2020-12-07 20:55:28 +0100

    Adds solution for day 5 part 2

--- day5/src/main/scala/Main.scala
+++ day5/src/main/scala/Main.scala
@@ -3,9 +3,21 @@ import scala.io.Source
 object Main {
   def main(args: Array[String]): Unit = {
     val input = Source.fromResource("input.txt").getLines().toList
-    val maxId = input.map(l => l.splitAt(7)).map{case (rowId, seatId) => calculateRowNumber(rowId.toList) * 8 + calulateSeatNumber(seatId.toList)}.max
+    val seatIds = input.map(l => l.splitAt(7)).map{case (rowId, seatId) => calculateRowNumber(rowId.toList) * 8 + calulateSeatNumber(seatId.toList)}
 
-    println(maxId)
+    println(seatIds.max)
+
+    // Part 2
+    println(findGapInIntList(seatIds))
+
+  }
+
+  def findGapInIntList(xs: List[Int]): Int = {
+
+    xs.sorted match {
+      case Nil => -1
+      case y :: ys => if ((ys.head - y) > 1) ys.head - 1 else findGapInIntList(ys)
+    }
   }
 
   def calculateRowNumber(rowId: List[Char]) = calculateItemNumber(rowId, 'F', 'B')
@@ -13,7 +25,6 @@ object Main {
 
   def calculateItemNumber(itemId: List[Char], firstHalfChar: Char, secondHalfChar: Char) = {
     def calculateRowNumberInRange(rowRange: Range, rowId: List[Char]): Int = {
-      println(rowRange)
       rowId match {
         case Nil => rowRange.end
         case x :: xs => if (x == firstHalfChar) calculateRowNumberInRange(rowRange.firstHalf(), xs) else calculateRowNumberInRange(rowRange.secondHalf(), xs)
@@ -21,6 +32,7 @@ object Main {
     }
     calculateRowNumberInRange(Range(0, (math.pow(2, itemId.size) - 1).toInt), itemId)
   }
+
 }
 case class Range(start: Int, end: Int) {
   def middleElement(): Int = {
