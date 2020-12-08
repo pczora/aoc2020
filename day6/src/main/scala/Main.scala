fad1cbefdb2873900d5728afeb35b88750db4227
Philipp Czora <philipp@czora.dev>
2020-12-08 08:59:42 +0100

    Adds solution for day 6 part 2

--- day6/src/main/scala/Main.scala
+++ day6/src/main/scala/Main.scala
@@ -1,39 +1,45 @@
+import scala.annotation.tailrec
 import scala.io.Source
 
 object Main {
   def main(args: Array[String]): Unit = {
     val input = Source.fromResource("input.txt").getLines.toList
-    println(groupNumbers(input).sum)
+    val groups = groupList(input)
+    val part1 = groups.map(group => group.foldLeft("")((z, l) => (z + l).distinct)).map(_.length).sum
+    println(part1)
+    
+    val part2 = groups.map {
+      case Nil => Nil
+      case x :: xs =>
+        commonChars(x, xs)
+    }.map(_.size).sum
+    println(part2)
   }
 
-  def groupNumbers(input: List[String]) = {
-    def groupNumbersAcc(acc: List[Int], input: List[String]): List[Int] = {
-      input match {
-        case Nil => acc
-        case x :: xs => {
-          val s = input.span(l => !l.isBlank)
-          s match {
-            case (group, groups) => {
-              if (!group.isEmpty) {
-                val groupUniqueChars = uniqueChars(group.foldLeft("")((z, l) => z + l)).size
-                groupNumbersAcc(groupUniqueChars :: acc, groups)
-              } else {
-                groupNumbersAcc(acc, groups.drop(1))
-              }
-            }
-          }
-        }
+  def groupList(input: List[String]): List[List[String]] = {
+    @tailrec
+    def groupListAcc(acc: List[List[String]], input: List[String]): List[List[String]] = {
+      if (input.isEmpty) acc else {
+       val s = input.span(l => !l.isBlank)
+       s match {
+         case (group, groups) =>
+           if (group.nonEmpty) groupListAcc(group :: acc, groups) else groupListAcc(acc, groups.drop(1))
+       }
       }
     }
-    groupNumbersAcc(List(), input)
+    groupListAcc(List(), input)
   }
-  def uniqueChars(input: String) = {
-    def uniqueCharsAcc(acc: List[Char], input: List[Char]): List[Char] = {
-      input match {
+
+
+  def commonChars(s: String, xs: List[String]): List[Char] = {
+    @tailrec
+    def commonCharsAcc(acc: List[Char], s: List[Char], xs: List[String]): List[Char] = {
+      s match {
         case Nil => acc
-        case x :: xs => if (acc.contains(x)) uniqueCharsAcc(acc, xs) else uniqueCharsAcc(acc ::: List(x), xs)
+        case y :: ys => if (xs.forall(x => x.contains(y))) commonCharsAcc(y :: acc, ys, xs) else commonCharsAcc(acc, ys, xs)
       }
     }
-    uniqueCharsAcc(List(), input.toList)
+    commonCharsAcc(List(), s.toList, xs)
   }
+
 }
